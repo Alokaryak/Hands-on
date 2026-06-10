7e287cbdf3adc47f5dcbf3125ba9ec29dc452201
Alokaryak <alokaryak98@gmail.com>
2026-06-10 22:09:09 +0530

    Java Basics

---
+++ TuteDude/Java-TuteDude/java_tuteDude/src/test/java/com/example/java_tuteDude/JavaTuteDudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_tuteDude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTuteDudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
